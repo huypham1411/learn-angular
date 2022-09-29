67ca3d956177093a5bd5a2b33603004530381f1b
Huy Pham Quoc <huypq@innotech-vn.com>
2022-09-29 15:13:28 +0700

    update new change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  public name = 'K';
+  public value = '';
+  onClick() {
+    console.log('save');
+  }
 }
