1409904b88f379ace6e95588e336640e7afd7d70
Huy Pham Quoc <huypq@innotech-vn.com>
2022-10-04 14:55:25 +0700

    add calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,12 @@ export class AppComponent {
   title = 'my-app';
   public name = 'K';
   public value = '';
+  public var1 = 0;
+  var2 = 0;
+  public sum = 0;
+  handleCalculateSum() {
+    return (this.sum = this.var1 + this.var2);
+  }
   onClick() {
     this.name = 'L';
   }
